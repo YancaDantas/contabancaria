764785fdfb5f22761980c68dd5e4fe5bf58f67f8
YancaDantas <160765209+YancaDantas@users.noreply.github.com>
2024-06-14 17:32:55 -0300

    Menu.java

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,19 +2,50 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
+import conta.util.Cores;
+
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+
+		// Teste da Classe Conta
+		Conta c1 = new Conta(1, 005, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+        
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(1, 563, 1, "Lucas Brito", 50.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+        // Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Helena Souza", 100000.0f, 15);
+		cp1.visualizar();
+        cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 		
 		Scanner leia = new Scanner(System.in);
 		int opcao;
 		
 		while (true) {
 			
-			System.out.println("*************************************************");
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+ "*************************************************");
 			System.out.println("                                                 ");
-			System.out.println("            BRANCO DO BRAZIL COM Z               ");
+			System.out.println("                BANCO DO BRAZIL COM Z            ");
 			System.out.println("                                                 ");
 			System.out.println("*************************************************");
 			System.out.println("                                                 ");
@@ -35,54 +66,51 @@ public class Menu {
 			opcao = leia.nextInt();
 			
 			if (opcao == 9) {
-				System.out.println("\nBanco do Brazil com z - O seu Futuro começa aqui!");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
 				sobre();
 				leia.close();
 				System.exit(0);
+			}
 				
-				switch (opcao) {
+			switch (opcao) {
 				case 1:
-					System.out.println("Criar Conta\n\n");
-					break;
-					
+					System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
+				
+                    		break;
 				case 2:
-					System.out.println("Listar todas as Contas\n\n");
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
 					
+                    		break;
 				case 3:
-					System.out.println("Cosultar dados da Conta - por número\n\n");
-					break;
-					
+					System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
+	
+                    		break;
 				case 4:
-					System.out.println("Atualizar dados da Conta\n\n");
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
 					
+                    		break;
 				case 5:
-					System.out.println("Apagar a Conta\n\n");
-					break;
-					
+					System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
+		
+                   	 	break;
 				case 6:
-					System.out.println("Saque\n\n");
-					break;
-					
+					System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
+
+                   	 	break;
 				case 7:
-					System.out.println("Depósito\n\n");
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
 					
+                    		break;
 				case 8:
-					System.out.println("Transferência entre Contas\n\n");
-					break;
+					System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
 					
+                    		break;
 				default:
-					System.out.println("\nOpção Inválida!\n");
-					break;
-					
-					
-				}
+					System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+                    		break;
 			}
-			
-			}
-		}
+		}	
+    }
 
 	private static void sobre() {
 		System.out.println("\n*****************************************");
